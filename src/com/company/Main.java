6dd324a0c2f778ed9364f82ded74f93a45e8ada8
Black-Loli <pundanya@gmail.com>
2022-03-25 15:28:59 +0300

    Финальная (наверное) версия задания

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,19 +17,7 @@ public class Main {
         cats_students.add(new Student("Пупкина", "Маргарита", 15, 437));
         cats_students.add(new Student("Тарасов", "Слонив", 50, 1433));
         cats_students.add(new Student("Меевич", "Адс", 14, 437));
-        cats_students.stream().filter(new Predicate<Student>() {
-                    @Override
-                    public boolean test(Student cat) {
-                        return !cat.Name.equals("Адс");
-                    }
-                }).sorted(new Comparator<Student>() {
-                    @Override
-                    public int compare(Student o1, Student o2) {
-                        return o1.Name.compareTo(o2.Name);
-                    }
-                })
+        cats_students.stream().filter(cat -> !cat.Name.equals("Адс")).sorted((o1, o2) -> o1.Name.compareTo(o2.Name))
                 .forEach(cat_stud -> System.out.println(cat_stud));
-
-        //System.out.println(cats_students);
     }
 }
